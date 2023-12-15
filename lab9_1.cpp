d3a1b2bd52594a1be4d8efd5cf28ebb4e3586772
Guy <guy_zxc@hotmail.com>
2023-12-15 15:09:53 +0700

    ลองใช้ฟังก์ชัน

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,6 +2,28 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
+void PayLoan(double Loan,double InT,double Pay){
+	cout << setw(13) << left << "EndOfYear#"; 
+	cout << setw(13) << left << "PrevBalance"; 
+	cout << setw(13) << left << "Interest"; 
+	cout << setw(13) << left << "Total";
+	cout << setw(13) << left << "Payment";
+	cout << setw(13) << left << "NewBalance";
+	cout << "\n";
+	for(int i=1 ;true ;i++){
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << i; 
+		cout << setw(13) << left << Loan;
+		cout << setw(13) << left << Loan*InT/100;
+		cout << setw(13) << left << Loan*InT/100+Loan;
+		if((Loan*InT/100+Loan)<=Pay)Pay=Loan*InT/100+Loan;
+		cout << setw(13) << left << Pay;
+		cout << setw(13) << left << (Loan*InT/100+Loan)-Pay;
+		cout << "\n";
+		Loan=(Loan*InT/100+Loan)-Pay;
+		if(Loan<=0)break;
+	}	
+}
 
 int main(){	
 	double Loan,InT,Pay;
@@ -11,7 +33,7 @@ int main(){
 	cin >> InT ;
 	cout << "Enter amount you can pay per year: ";
 	cin >> Pay ; 
-
+	/*
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -25,19 +47,20 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	for(int i=1 ;true ;++i){
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << i; 
-	cout << setw(13) << left << Loan;
-	cout << setw(13) << left << Loan*InT/100;
-	cout << setw(13) << left << Loan*InT/100+Loan;
-	if((Loan*InT/100+Loan)<=Pay)Pay=Loan*InT/100+Loan;
-	cout << setw(13) << left << Pay;
-	cout << setw(13) << left << (Loan*InT/100+Loan)-Pay;
-	cout << "\n";
-	Loan=(Loan*InT/100+Loan)-Pay;
-	if(Loan<=0)break;
-	}	
+	for(int i=1 ;true ;i++){
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << i; 
+		cout << setw(13) << left << Loan;
+		cout << setw(13) << left << Loan*InT/100;
+		cout << setw(13) << left << Loan*InT/100+Loan;
+		if((Loan*InT/100+Loan)<=Pay)Pay=Loan*InT/100+Loan;
+		cout << setw(13) << left << Pay;
+		cout << setw(13) << left << (Loan*InT/100+Loan)-Pay;
+		cout << "\n";
+		Loan=(Loan*InT/100+Loan)-Pay;
+		if(Loan<=0)break;
+	}*/
+	PayLoan(Loan,InT,Pay);	
 	
 	return 0;
 }
